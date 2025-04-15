7fe394b9750f051c52fcad2081dab6fd3555f775
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-04-14 22:26:34 -0600

    Add templateUrl and differents methos in ts

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -1,15 +1,23 @@
 import { Component } from "@angular/core";
 
 @Component({
-  template:`
+  // template:`
 
-    <h1>Hi </h1>
-    <p>Counter page {{ counter }}</p>
-    <!-- <button (click)="counter = counter + 1">Increment</button> -->
-    <button (click)="increaseBy(1)">Increment</button>
-    <button (click)="decreaseBy(1)">Decrement</button>
+  //   <h1>Hi </h1>
+  //   <p>Counter page {{ counter }}</p>
+  //   <!-- <button (click)="counter = counter + 1">Increment</button> -->
+  //   <button (click)="increaseBy(1)">Increment</button>
+  //   <button (click)="decreaseBy(1)">Decrement</button>
 
-    `
+  //   `
+  templateUrl: './counter-page.component.html',
+  styles: `
+     button {
+      padding: 5px;
+      margin: 5px 10px;
+      with: 75px;
+     }
+  `
 })
 export class CounterPageComponent {
 
@@ -20,6 +28,9 @@ export class CounterPageComponent {
   }
   decreaseBy(value: number) {
     this.counter -= value;
+  }
+  reset(){
+    this.counter = 0;
   }
 
 }
